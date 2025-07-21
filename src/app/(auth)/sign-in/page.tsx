7659810c7b632d3b5fecdeee1f825bf14dfdce37
chariff16 <lotfibusi@gmail.com>
2025-07-21 20:42:39 +0100

    creating sign in page

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+import React from 'react'
+import SignInCard from '@/features/auth/components/sign-in-card'
+
+const SignIn = () => {
+    return (
+        <>
+            <SignInCard />
+        </>
+    )
+}
+
+export default SignIn
---
+++ src/features/auth/components/sign-in-card.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import DottedSeprator from '@/components/dotted-seprator';
+import { Button } from '@/components/ui/button';
+
+const SignInCard = () => {
+    return (
+        <Card className='w-full h-full md:w-[487px] border-none shadow-none '>
+            <CardHeader className='flex items-center justify-center text-center p-7'>
+                <CardTitle className='text-2xl'>
+                    hello man
+                </CardTitle>
+            </CardHeader>
+            <div className='px-7'>
+                <DottedSeprator />
+            </div>
+            <CardContent className='p-7'>
+                <form className='space-y-4'>
+                    <Input
+                        required
+                        type='email'
+                        value={""}
+                        onChange={() => { }}
+                        placeholder='Enter Email Address'
+                        disabled={false}
+                    />
+                    <Input
+                        required
+                        type='password'
+                        value={""}
+                        onChange={() => { }}
+                        placeholder='Enter Password'
+                        disabled={false}
+                        min={8}
+                        max={256}
+                    />
+                    <Button disabled={false} size='lg' className='w-full'>
+                        Login
+                    </Button>
+                </form>
+            </CardContent>
+            <div className='px-7'>
+                <DottedSeprator />
+            </div>
+            <CardContent className='px-7 flex flex-col gap-y-4'>
+                <Button disabled={false} variant='secondary' size='lg' className='w-full' >
+                    <FcGoogle className='mr-2 size-5' />
+                    Login with Google
+                </Button>
+                <Button disabled={false} variant='secondary' size='lg' className='w-full' >
+                    <FaGithub className='mr-2 size-5' />
+                    Login with GitHub
+                </Button>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default SignInCard
