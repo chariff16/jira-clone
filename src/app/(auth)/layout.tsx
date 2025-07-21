cb3148e03b50fddea6e9a50aab8c27512a0598e0
chariff16 <lotfibusi@gmail.com>
2025-07-22 00:06:05 +0100

    adding push routing to button

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+'use client'
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 interface AuthProps {
     children: React.ReactNode
 }
 
-const layout = ({ children }: AuthProps) => {
+const Authlayout = ({ children }: AuthProps) => {
+    const pathName = usePathname();
+
     return (
         <main className='bg-neutral-100 min-h-screen'>
             <div className='mx-auto max-w-screen-2xl p-4'>
@@ -17,8 +21,10 @@ const layout = ({ children }: AuthProps) => {
                         width={152}
                         alt='logo image'
                     />
-                    <Button variant="secondary">
-                        Sing up
+                    <Button asChild variant="secondary">
+                        <Link href={pathName === '/sign-in' ? '/sign-up' : '/sign-in'}>
+                            {pathName === '/sign-in' ? 'Sing up' : 'Login'}
+                        </Link>
                     </Button>
                 </nav>
                 <div className='flex flex-col items-center justify-center pt-4 md:pt-14 gap-2'>
@@ -30,4 +36,4 @@ const layout = ({ children }: AuthProps) => {
     )
 }
 
-export default layout
+export default Authlayout
