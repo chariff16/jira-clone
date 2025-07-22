9adf134002aa8dc61fa496c4ab46b85e768807b6
chariff16 <lotfibusi@gmail.com>
2025-07-22 17:34:28 +0100

    creating register auth api

--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -3,4 +3,10 @@ import { z } from 'zod';
 export const loginSchema = z.object({
     email: z.string().email(),
     password: z.string()
-})
+});
+
+export const registerSchema = z.object({
+    name: z.string().trim().min(2, "Require 2 charaters"),
+    email: z.string().email(),
+    password: z.string().min(8, "Minimum of 8 charecter"),
+});
