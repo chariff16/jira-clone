88e9f1c54ac6afd23f2100719c9a5934b0921da3
chariff16 <lotfibusi@gmail.com>
2025-07-22 23:47:11 +0100

    createing session and middelware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
 
-
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  const logout = () => {
+    mutate();
+    // router.push("/sign-in");
+  }
+
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data])
   return (
     <div className="">
-      hello man
-      <Button variant="default" size="sm">
-        click Me!
+      autherized users
+      <Button onClick={() => logout()}>
+        logout
       </Button>
     </div>
   );
