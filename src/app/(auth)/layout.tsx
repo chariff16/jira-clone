fd8263deb5a04fedf62e9f753d9bef9a18b9ed3e
chariff16 <lotfibusi@gmail.com>
2025-07-23 14:28:33 +0100

    adding link the logo

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -15,12 +15,14 @@ const Authlayout = ({ children }: AuthProps) => {
         <main className='bg-neutral-100 min-h-screen'>
             <div className='mx-auto max-w-screen-2xl p-4'>
                 <nav className='flex justify-between items-center'>
-                    <Image
-                        src='/logo.svg'
-                        height={56}
-                        width={152}
-                        alt='logo image'
-                    />
+                    <Link href="/">
+                        <Image
+                            src='/logo.svg'
+                            height={56}
+                            width={152}
+                            alt='logo image'
+                        />
+                    </Link>
                     <Button asChild variant="secondary">
                         <Link href={pathName === '/sign-in' ? '/sign-up' : '/sign-in'}>
                             {pathName === '/sign-in' ? 'Sing up' : 'Login'}
