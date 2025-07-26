189ec23a5299368420278b3bee2297b2c7ebef27
chariff16 <lotfibusi@gmail.com>
2025-07-26 17:29:30 +0100

    creating workspaces actions

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+'use client';
+import { useParams } from "next/navigation";
 
 const WorkspaceIdPage = () => {
+    const params = useParams();
+
     return (
         <div>
-            workspace id
+            workspace id {params.workspaceId}
         </div>
     )
 }
