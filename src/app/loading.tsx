9b8736fef5f2697de3cfa8f730365c3e6805f4f5
chariff16 <lotfibusi@gmail.com>
2025-08-09 20:52:28 +0100

    adding glabal loading

---
+++ src/app/loading.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { Loader } from "lucide-react";
+
+
+const LoadingPage = () => {
+    return (
+        <div className="min-h-screen flex flex-col items-center justify-center gap-y-2">
+            <Loader className="size-6 animate-spin text-muted-foreground" />
+
+        </div>
+    )
+}
+
+export default LoadingPage;
