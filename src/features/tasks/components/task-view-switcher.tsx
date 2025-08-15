a535270a346104c3f237a5c810ef4094b78058eb
chariff16 <lotfibusi@gmail.com>
2025-08-15 03:35:29 +0100

    creating calendar and calendar cards ui

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -15,6 +15,7 @@ import DataKanban from "./data-kanban";
 import { useCallback } from "react";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTask } from "../api/use-bulk-update-task";
+import DataCalendar from "./data-calendar";
 
 const TaskViewSwitcher = () => {
     const [view, setView] = useQueryState("task-view", {
@@ -68,8 +69,8 @@ const TaskViewSwitcher = () => {
                             <TabsContent value="kanban" className="mt-0">
                                 <DataKanban onChange={onKanbanChange} data={tasks?.documents ?? []} />
                             </TabsContent>
-                            <TabsContent value="calendar" className="mt-0">
-                                {JSON.stringify(tasks)}
+                            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+                                <DataCalendar data={tasks?.documents ?? []} />
                             </TabsContent>
                         </>
 
